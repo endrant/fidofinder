c714cb0d40f94e68affcd0c223c97fc2dc2f35f0
Chele B <DesertBell@users.noreply.github.com>
2018-10-27 10:30:31 -0700

    Delete rootRed.js

--- src/reducers/rootRed.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import {alert} from './alertsRed';
-import {authentication} from './authRed';
-import {registration} from './regRed';
-import {users} from './usersRed';
-import {submitForm} from './submitRed';
-import {dogs} from './dogRed'
-export const rootRed = combineReducers({alert, authentication, registration, users, submitForm,  dogs});
